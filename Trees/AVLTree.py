4dbaed7e1559cfad87c31f40b66f0572a07195a5
selina-28030 <54951726+selina-28030@users.noreply.github.com>
2020-04-16 11:36:21 -0700

    Update AVLTree.py

--- Trees/AVLTree.py
+++ Trees/AVLTree.py
@@ -7,53 +7,43 @@ from Trees.BinaryTree import BinaryTree, Node
 from Trees.BST import BST
 
 class AVLTree(BST):
-    '''
-    FIXME:
-    AVLTree is currently not a subclass of BST.
-    You should make the necessary changes in the class declaration line above 
-    and in the constructor below.
-    '''
-    def __init__(self, xs=None):
-        '''
-        FIXME:
-        Implement this function.
-        '''
-	super().__init__()
-	if xs:
-		self.insert_list(xs)
+	def __init__(self, xs=None):
+		
+		super().__init__()
+        	if xs:
+            		self.insert_list(xs)
 
-
-    def balance_factor(self):
+	def balance_factor(self):
         '''
         Returns the balance factor of a tree.
         '''
-        return AVLTree._balance_factor(self.root)
+        	return AVLTree._balance_factor(self.root)
 
-    @staticmethod
-    def _balance_factor(node):
-        '''
-        Returns the balance factor of a node.
-        '''
-        if node is None:
-            return 0
-        return BinaryTree._height(node.left) - BinaryTree._height(node.right)
+	@staticmethod
+	def _balance_factor(node):
+	'''
+	Returns the balance factor of a node.
+	'''
+		if node is None:
+	    		return 0
+		return BinaryTree._height(node.left) - BinaryTree._height(node.right)
 
 
-    def is_avl_satisfied(self):
+	def is_avl_satisfied(self):
         '''
         Returns True if the avl tree satisfies that all nodes have a balance factor in [-1,0,1].
         '''
-        return AVLTree._is_avl_satisfied(self.root)
+        	return AVLTree._is_avl_satisfied(self.root)
 
-    @staticmethod
-    def _is_avl_satisfied(node):
+	@staticmethod
+    	def _is_avl_satisfied(node):
         '''
         FIXME:
         Implement this function.
         '''
-	if node is None:
-		return True
-	return AVLTree._balance_factor(node) in [-1, 0, 1] and AVLTree._is_avl_satisfied(node.left) and AVLTree._is_avl_satisfied(node.right)
+		if node is None:
+			return True
+		return AVLTree._balance_factor(node) in [-1, 0, 1] and AVLTree._is_avl_satisfied(node.left) and AVLTree._is_avl_satisfied(node.right)
 
     @staticmethod
     def _left_rotate(node):
